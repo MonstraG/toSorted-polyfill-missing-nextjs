b7d7a3b469b2daba81414610be2497a683d02e9b
Arseny Garelyshev <arseny@lifekeys.no>
2023-11-09 10:03:58 +0100

    add repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return [3,1,2,4].toSorted().toSpliced(1, 2).toReversed().map(x => <div key={x}>{x}</div>)
 }
